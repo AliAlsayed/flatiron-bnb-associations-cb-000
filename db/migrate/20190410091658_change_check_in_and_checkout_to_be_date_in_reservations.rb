02ee33856729fbee084528ceb4772a2cbec579bd
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-10 09:19:53 +0000

    Automatically backed up by Learn

--- db/migrate/20190410091658_change_check_in_and_checkout_to_be_date_in_reservations.rb
+++ db/migrate/20190410091658_change_check_in_and_checkout_to_be_date_in_reservations.rb
@@ -1,6 +1,6 @@
 class ChangeCheckInAndCheckoutToBeDateInReservations < ActiveRecord::Migration[5.0]
   def change
-    change_column :reservations, :check_in, :date
-    change_column :reservations, :check_out, :date
+    change_column :reservations, :checkin, :date
+    change_column :reservations, :checkout, :date
   end
 end
